f4351bcb6864c28cc29d1098bce770ff0906bc8b
Brian Camp <brian@thecamps.org>
2018-01-13 15:11:41 -0600

    Add a default PATH to work around the case of a missing or incomplete PATH

--- vars.bash
+++ vars.bash
@@ -1,1 +1,6 @@
+# Name used to identify rig
 export me="$(hostname -s | sed -r 's/[^0-9a-zA-Z]//g')"
+
+# Append these default paths, incase we're running from an init
+# that doesn't.
+export PATH="$PATH":/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
